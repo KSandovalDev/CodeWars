43f0259d45315516c98579030fcc8d41d23ef545
Kevin Sandoval <ksandovaldev@gmail.com>
2021-02-12 18:33:24 -0500

    codewars 8kyu

---
+++ Week 5/bonus.js
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/56f6ad906b88de513f000d96
+
+function bonusTime(salary, bonus) {
+
+  
+  
+  
+  
+    // your code here
+    if(bonus === true) return `£${salary * 10}`
+      else return `£${salary}`
+  
+  }
---
+++ Week 5/quarter.js
@@ -0,0 +1,20 @@
+// https://www.codewars.com/kata/5ce9c1000bab0b001134f5af
+
+// const quarterOf = (month) => {
+  // Your code here
+//   Parameter - Month
+//   Return - number or quarter of the year
+//   Example - march would be in the 3rd quarter of the year
+//   Pseudo code - 
+// 
+
+function quarterOf(month){
+    if (month <=3){
+      return 1
+    }else if (month <=6){
+      return 2
+    }else if (month <=9){
+      return 3
+    }else return 4
+    
+  }
