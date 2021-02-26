8fa3f7da2f72f1dbe2e06105d5c2193b0e3e9372
KSandovalDev <ksandovaldev@gmail.com>
2021-02-25 23:54:11 -0500

    Create grasshoppersum.js

---
+++ Javascript/grasshoppersum.js
@@ -0,0 +1,10 @@
+https://www.codewars.com/kata/55d24f55d7dd296eb9000030/train/javascript
+
+var summation = function (num) {
+  let result = 0;
+  for (var i = 1; i <= num; i++) {
+    result += i;
+  }
+  
+  return result;
+}
