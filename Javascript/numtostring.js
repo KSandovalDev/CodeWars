f7555f6243809fc0d49439cd8b1918b681a3fe6e
KSandovalDev <ksandovaldev@gmail.com>
2021-03-01 22:45:43 -0500

    Create numtostring.js

---
+++ Javascript/numtostring.js
@@ -0,0 +1,7 @@
+//https://www.codewars.com/kata/5265326f5fda8eb1160004c8/train/javascript
+
+function numberToString(num) {
+  return num.toString()
+}
+
+// code was asking to convert a number to a string
