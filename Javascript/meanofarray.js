1f0ed7d4e15b962c8f78342cb939fc32abb9023f
KSandovalDev <ksandovaldev@gmail.com>
2021-03-16 22:35:05 -0400

    Create meanofarray.js

---
+++ Javascript/meanofarray.js
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/563e320cee5dddcf77000158/train/javascript
+
+function getAverage(marks){
+  let x = marks.reduce((accumulator, currentValue) => accumulator + currentValue)
+  return parseInt(x/marks.length)
+}
