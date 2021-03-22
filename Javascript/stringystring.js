009f1daadbcce37e44806c0231de654656777851
KSandovalDev <ksandovaldev@gmail.com>
2021-03-21 23:17:36 -0400

    Create stringystring.js

---
+++ Javascript/stringystring.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/563b74ddd19a3ad462000054/train/javascript
+
+function stringy(size) {
+  let str = '';
+  for (let i = 1; i <= size; i++) {
+    str = str + i%2   
+  }
+  return str;
+}
