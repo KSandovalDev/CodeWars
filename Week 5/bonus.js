c041ac322fadf09b93232aed85309619ed8d3309
KSandovalDev <ksandovaldev@gmail.com>
2021-04-02 22:43:28 -0400

    Update bonus.js

--- Week 5/bonus.js
+++ Week 5/bonus.js
@@ -1,11 +1,6 @@
 // https://www.codewars.com/kata/56f6ad906b88de513f000d96
 
 function bonusTime(salary, bonus) {
-
-  
-  
-  
-  
     // your code here
     if(bonus === true) return `£${salary * 10}`
       else return `£${salary}`
