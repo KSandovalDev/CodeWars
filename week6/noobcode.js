6197bcd9e9f082f9c9c286b0fa78d424370dd819
KSandovalDev <ksandovaldev@gmail.com>
2021-04-11 23:32:58 -0400

    Update noobcode.js

--- week6/noobcode.js
+++ week6/noobcode.js
@@ -1,5 +1,7 @@
 // https://www.codewars.com/kata/5709bdd2f088096786000008/train/javascript
 
+
+
 function superSize(num){
     return parseInt(num.toString().split('').sort().reverse().join(''))
   }
