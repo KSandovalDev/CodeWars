95efc2a4d0fadb1ca2e5ffa4fafd77898dc63fb3
KSandovalDev <ksandovaldev@gmail.com>
2021-06-01 23:43:42 -0400

    Update reversestring.js

--- Javascript/reversestring.js
+++ Javascript/reversestring.js
@@ -6,7 +6,6 @@ https://www.codewars.com/kata/5168bb5dfe9a00b126000018
 // P -psuedo code - 
 // we want to split the string into an array of strings and then reverse their order and then join them again
 
-
 function solution(str){
   return str.split('').reverse().join('')
 //   we're using an extra space because in this case it will return the whole string
